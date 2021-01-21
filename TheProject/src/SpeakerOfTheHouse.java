e8b9eb5abbe6bbc06a5a50fb507644c34d0cce2d
Owen Halbrook <33768369+odhalbrook@users.noreply.github.com>
2021-01-21 08:31:45 -0700

    Update SpeakerOfTheHouse.java

--- TheProject/src/SpeakerOfTheHouse.java
+++ TheProject/src/SpeakerOfTheHouse.java
@@ -1,10 +1,7 @@
 
 public final class SpeakerOfTheHouse extends Representative{
 
-	public final int getMyDistrictNum()
-	{
-		return 0;
-	}
+	
 	public final void getSecurityClearance() {
 		// TODO Auto-generated method stub
 		
@@ -13,6 +10,10 @@ public final class SpeakerOfTheHouse extends Representative{
 	public void sayMyProfession()
 	{
 		System.out.println("I am the leader of the House of Representatives");
+	}
+	public void bangGavel()
+	{
+		System.out.println("The house will be in order.");
 	}
 	
 }
