e79d77dcca31e1d65dc8037205f29d3a6fff98be
Owen Halbrook <33768369+odhalbrook@users.noreply.github.com>
2021-01-20 21:17:30 -0700

    Update Representative.java

--- TheProject/src/Representative.java
+++ TheProject/src/Representative.java
@@ -1,5 +1,10 @@
 
 public abstract class Representative extends PublicOffcial {
 
-	
+	public abstract int getMyDistrictNum();
+	public final int getNumOfTermsServed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
